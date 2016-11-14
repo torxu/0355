4fc8459db69083486e6ad292d6a23acedb34a5fb
torxu <aitor.otero.dam1@iessanandres.com>
2016-11-14 21:21:51 +0100

    0355.b Añadida funcionalidad para fijar fecha y hora, mostrar fecha y hora y avanzar fecha y hora

--- DisplayHoraYFecha.java
+++ DisplayHoraYFecha.java
@@ -7,27 +7,53 @@
  */
 public class DisplayHoraYFecha
 {
-    // instance variables - replace the example below with your own
-    private ClockDisplay reloj;
-    private CalendarioAitor fecha;
-    private CalendarioAitor momento;
     private NumberDisplay horas;
     private NumberDisplay minutos;
+    private DisplayDosDigitos dia;
+    private DisplayDosDigitos mes;
+    private DisplayDosDigitos año;
 
     /**
      * Constructor for objects of class DisplayHoraYFecha
      */
     public DisplayHoraYFecha()
     {
-        fecha = new CalendarioAitor();
-        reloj = new ClockDisplay();
+        horas = new NumberDisplay(24);
+        minutos = new NumberDisplay(60);
+        dia = new DisplayDosDigitos(31);
+        mes = new DisplayDosDigitos(13);
+        año = new DisplayDosDigitos(100);
     }
     
     public void avanzarMomento()
     {
-        reloj.avanzarMinuto();
-        if(horas.getValue() == 0 && minutos.getValue() == 0){
-            fecha.avanzarFecha();
+        minutos.increment();
+        if(minutos.getValue() == 0){
+           horas.increment();
+           if(horas.getValue() == 0 && minutos.getValue() == 0){
+            dia.incrementaValor();
+            if (dia.getValor() == 1);{
+                mes.incrementaValor();
+                if(mes.getValor() == 1);{
+                    año.incrementaValor();
+                }
+            }
+           }
         }
+        
+    }
+    
+    public void setMomento(int h, int m, int di, int me, int añ)
+    {
+       horas.setValue(h);
+       minutos.setValue(m);
+       dia.setValor(di);
+       mes.setValor(me);
+       año.setValor(añ);
+    }
+    
+    public String getMomento()
+    {
+        return horas.getDisplayValue()+":"+minutos.getDisplayValue()+" "+dia.getValorDelDisplay()+"/"+mes.getValorDelDisplay()+"/"+año.getValorDelDisplay();
     }
 }
